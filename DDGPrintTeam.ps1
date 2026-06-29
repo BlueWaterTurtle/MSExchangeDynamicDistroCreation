5828bc0635809b56254bca0169d3dacf595533ca
Todd Bonner <toddkbonner@yahoo.com>
2026-06-29 16:53:52 -0400

    Update team name in DDGPrintTeam.ps1 script

--- DDGPrintTeam.ps1
+++ DDGPrintTeam.ps1
@@ -1,7 +1,7 @@
 ﻿#Use this script to export a .csv containing the DDG's member Email addresses and names. this is then referenced in the "createDDG.ps1" script. Currently the .csv still needs to be manually formated/checked beforehand. 
 
 
-$ddg = Get-DynamicDistributionGroup -Identity "TeamKnameDynamic" #change the team name as needed
+$ddg = Get-DynamicDistributionGroup -Identity "TeamnameDynamic" #change the team name as needed
 
 
 $outPath = Join-Path $HOME "Teamname-members.csv"  
